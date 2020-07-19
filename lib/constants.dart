20dae291d60773779481131b2a842029bf80596b
shivani mutke <42477972+shivanimutke@users.noreply.github.com>
2020-07-19 23:18:18 +0530

    Add files via upload

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+const kAppTitle = 'flutter_todos';
+
+const kAboutPageUrl = 'http://ithinkdiff.net';
+const kAppPortfolioApple =
+    'https://apps.apple.com/us/developer/ithinkdiff/id377715733';
+const kappPortfolioGoogle =
+    'https://play.google.com/store/apps/developer?id=iThinkdiff';
+const kAppTwitterPage = 'https://twitter.com/ithinkdiff_apps';
+
+// AppStore and Google PlayStore URL
+const kAppStoreUrl = '';
+const kGooglePlayUrl = '';
